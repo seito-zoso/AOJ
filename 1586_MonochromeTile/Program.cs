2a144028eea3c9b108fa958ac6eca11d6f5dd491
Seito Shirata <seito.shirata@omron.com>
2022-08-26 13:19:22 +0900

    Delete Input and use tuple

--- 1586_MonochromeTile/Program.cs
+++ 1586_MonochromeTile/Program.cs
@@ -8,40 +8,20 @@ namespace _1586_MonochromeTile
     {
         private static void Main(string[] args)
         {
-            var inputs = GetInputs();
-            var outputs = GetOutputs(inputs);
+            var (tileSize, checkAreas)  = GetTileSizeAndCheckAreas();
+            var outputs = CreateMonochromeTileAndCountBlackedTiles(tileSize, checkAreas);
             ShowOutputs(outputs);
         }
 
-        #region 内部クラス
-
-        private class Inputs
-        {
-            public Inputs(int width, int height, IEnumerable<int[]> checkAreas)
-            {
-                this.Width = width;
-                this.Height = height;
-                this.CheckAreas = checkAreas;
-            }
-
-            public int Width { get; }
-
-            public int Height { get; }
-
-            public IEnumerable<int[]> CheckAreas { get; }
-        }
-
-        #endregion
-
         #region 入力処理
 
-        private static Inputs GetInputs()
+        private static ((int width, int height), IEnumerable<int[]>) GetTileSizeAndCheckAreas()
         {
             //var (width, height) = GetRectangleSize();
             var (height, width) = GetRectangleSize(); // AOJ のテスト入力は高さ、横幅の順
             var numberOfDays = ReadIntegerNumber();
             var rectangleAreaCollection = GetRectangleAreaCollection(numberOfDays);
-            return new Inputs(width, height, rectangleAreaCollection);
+            return ((width, height), rectangleAreaCollection);
         }
 
         private static (int, int) GetRectangleSize()
@@ -77,63 +57,47 @@ namespace _1586_MonochromeTile
             return splitLine.Select(int.Parse).ToArray();
         }
 
-        private static Inputs GetSampleInputs()
-        {
-            return new Inputs(
-                5,
-                4,
-                new List<int[]>()
-                    {
-                        new [] { 1, 1, 3, 3 },
-                        new [] { 3, 2, 4, 2 },
-                        new [] { 4, 3, 5, 4 },
-                        new [] { 1, 4, 5, 4 },
-                        new [] { 4, 1, 4, 1 },
-                    });
-        }
-
         #endregion
 
         #region 演算処理
 
-        // ここの命名
-        private static IEnumerable<int> GetOutputs(Inputs inputs)
+        private static IEnumerable<int> CreateMonochromeTileAndCountBlackedTiles((int width, int height) tileSize, IEnumerable<int[]> checkAreas)
         {
-            // TileRectangleクラスは作らないようにした
-            var tiles = InitializeTiles(inputs.Width, inputs.Height);
-            return TaroActs(tiles, inputs.CheckAreas);
+            var whiteTiles = InitializeTiles(tileSize.width, tileSize.height, Color.White);
+            return PaintAllCheckAreasBlack(whiteTiles, checkAreas);
         }
 
-        private static Tile[,] InitializeTiles(int width, int height)
+        private static Tile[,] InitializeTiles(int width, int height, Color color)
         {
             var tiles = new Tile[width, height];
             for (var i = 0; i < height; i++)
             {
                 for (var j = 0; j < width; j++)
                 {
-                    tiles[i , j] = new Tile(Color.White);
+                    tiles[i , j] = new Tile(color);
                 }
             }
 
             return tiles;
         }
 
-        private static IEnumerable<int> TaroActs(
+        private static IEnumerable<int> PaintAllCheckAreasBlack(
             Tile[,] tiles,
             IEnumerable<int[]> checkAreas)
         {
-            var total = 0;
-            foreach (var checkAreaOneDay in checkAreas)
+            var totalCount = 0;
+            foreach (var checkArea in checkAreas)
             {
-                var oneDayCount = PaintTilesBlack(tiles, checkAreaOneDay);
-                yield return total += oneDayCount;
+                var blackedTileCount = PaintCheckAreaBlackTilesBlack(tiles, checkArea);
+                yield return totalCount += blackedTileCount;
             }
         }
 
-        private static int PaintTilesBlack(Tile[,] tiles, int[] checkAreaOneDay)
+        private static int PaintCheckAreaBlackTilesBlack(Tile[,] tiles, int[] checkArea)
         {
-            var targetTiles = GetTilesInArea(tiles, checkAreaOneDay).ToList();
-            if (CheckAllTilesColor(targetTiles, Color.White) == false)
+            var targetTiles = GetTilesInArea(tiles, checkArea).ToList();
+            var araAllTilesWhite = CheckAllTilesColor(targetTiles, Color.White);
+            if (araAllTilesWhite  == false)
             {
                 return 0;
             }
