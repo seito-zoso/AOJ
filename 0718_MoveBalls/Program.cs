61c41ee9437ed0a410de6167be10725ae0e03a5e
Seito Shirata <seito.shirata@omron.com>
2022-08-26 14:55:59 +0900

    Create new project 0718_MoveBalls.csproj

---
+++ 0718_MoveBalls/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _0718_MoveBalls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
