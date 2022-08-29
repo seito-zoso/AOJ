c1c85f5f8cc18cab0ecad76877bf094014f4a596
Seito Shirata <seito.shirata@omron.com>
2022-08-29 15:14:23 +0900

    Implement and Resolved.

--- 0718_MoveBalls/Program.cs
+++ 0718_MoveBalls/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _0718_MoveBalls
 {
@@ -6,7 +8,143 @@ namespace _0718_MoveBalls
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var (numberOfBalls, operationList) = GetNumberOfBallsAndOperationList();
+            var boxNumbersBallsIn = MoveBalls(numberOfBalls, operationList);
+            DisplayIntegerNumbers(boxNumbersBallsIn);
         }
+
+        #region 入力処理
+
+        private static (int numberOfBalls, IReadOnlyList<(int ballNumber, int boxNumber)> operationList) GetNumberOfBallsAndOperationList()
+        {
+            var (numberOfBalls, numberOfOperations) = ReadSpaceSeparatedTwoIntegerNumbers();
+            var operationList = GetOperationList(numberOfOperations);
+
+            return (numberOfBalls, operationList);
+        }
+
+        private static IReadOnlyList<(int ballNumber, int boxNumber)> GetOperationList(int numberOfOperations)
+        {
+            var operationList = new List<(int, int)>();
+            for (var i = 0; i < numberOfOperations; i++)
+            {
+                var operation = ReadSpaceSeparatedTwoIntegerNumbers();
+                operationList.Add(operation);
+            }
+
+            return operationList;
+        }
+
+        private static (int, int) ReadSpaceSeparatedTwoIntegerNumbers()
+        {
+            const string Space = " ";
+            var readLine = Console.ReadLine();
+            var splitLine = readLine.Split(Space);
+
+            return (int.Parse(splitLine[0]), int.Parse(splitLine[1]));
+        }
+
+        #endregion
+
+        #region 演算処理
+
+        private static IEnumerable<int> MoveBalls(int numberOfBalls, IReadOnlyList<(int ballNumber, int boxNumber)> operationList)
+        {
+            var boxes = InitializeBoxesContainingBalls(numberOfBalls);
+            foreach (var operation in operationList)
+            {
+                MoveBall(boxes, operation.ballNumber, operation.boxNumber);
+            }
+
+            return GetBoxNumbersBallsIn(numberOfBalls, boxes);
+        }
+
+        private static IList<Box> InitializeBoxesContainingBalls(int numberOfBalls)
+        {
+            var boxes = new List<Box>();
+
+            for (var i = 0; i < numberOfBalls; i++)
+            {
+                var ball = new Ball(i + 1); // ballのnewはここでいいの？
+                var box = new Box(i + 1);
+                box.Balls.Add(ball);
+                boxes.Add(box);
+            }
+
+            return boxes;
+        }
+
+        private static void MoveBall(IList<Box> boxes, int ballNumber, int boxNumber)
+        {
+            var (ball, fromBox) = FindBallAndContainingBox(boxes, ballNumber);
+            fromBox.Balls.Remove(ball);
+
+            var toBox = boxes[boxNumber - 1];
+            toBox.Balls.Add(ball);
+        }
+
+        private static (Ball, Box) FindBallAndContainingBox(IEnumerable<Box> boxes, int ballNumber)
+        {
+            Ball ball = null;
+            var containingBox = boxes.FirstOrDefault(
+                box =>
+                    {
+                        ball = box.Balls.FirstOrDefault(x => x.Number == ballNumber);
+                        return ball != null;
+                    });
+
+            return (ball, containingBox);
+        }
+
+        private static IEnumerable<int> GetBoxNumbersBallsIn(int numberOfBalls, IList<Box> boxes)
+        {
+            for (var i = 0; i < numberOfBalls; i++)
+            {
+                var ballNumber = i + 1;
+                yield return boxes.FirstOrDefault(box => box.Balls.Any(ball => ball.Number == ballNumber)).Number;
+            }
+        }
+
+        #endregion
+
+        #region 内部クラス
+
+        private class Box
+        {
+            public Box(int number)
+            {
+                this.Number = number;
+                this.Balls = new List<Ball>();
+            }
+
+            public int Number { get; }
+
+            public ICollection<Ball> Balls { get; }
+        }
+
+        private class Ball
+        {
+            public Ball(int number)
+            {
+                this.Number = number;
+            }
+
+            public int Number { get; }
+        }
+
+        #endregion
+
+        #region 出力処理
+
+        private static void DisplayIntegerNumbers(IEnumerable<int> numbers)
+        {
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+        #endregion
+
     }
 }
--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 
 PCソフトウェア塾 課題用レポジトリ
 
-| ID                                                                     | Name            | Submit | Date       |
-| ---------------------------------------------------------------------- | --------------- | ------ | ---------- |
-| [0078](https://onlinejudge.u-aizu.ac.jp/challenges/search/titles/0078) | Magic Square    |        | 2021-12-18 |
-| [0556](https://onlinejudge.u-aizu.ac.jp/challenges/search/titles/0556) | Tile            |        | 2022-06-24 |
-| [1586](https://onlinejudge.u-aizu.ac.jp/challenges/search/titles/1586) | Monochrome Tile |        | 2022-08-26 |
-| [0104](https://onlinejudge.u-aizu.ac.jp/challenges/search/titles/0104) | Magical Tiles   |        | 2022-06-24 |
-| [0718](https://onlinejudge.u-aizu.ac.jp/challenges/search/titles/0718) | Move Balls      |        | 2022-09-30 |
+| ID                                                                     | Name            | Submit                                                                                            | Date       |
+| ---------------------------------------------------------------------- | --------------- | ------------------------------------------------------------------------------------------------- | ---------- |
+| [0078](https://onlinejudge.u-aizu.ac.jp/challenges/search/titles/0078) | Magic Square    |                                                                                                   | 2021-12-18 |
+| [0556](https://onlinejudge.u-aizu.ac.jp/challenges/search/titles/0556) | Tile            |                                                                                                   | 2022-06-24 |
+| [1586](https://onlinejudge.u-aizu.ac.jp/challenges/search/titles/1586) | Monochrome Tile |                                                                                                   | 2022-08-26 |
+| [0104](https://onlinejudge.u-aizu.ac.jp/challenges/search/titles/0104) | Magical Tiles   |                                                                                                   | 2022-06-24 |
+| [0718](https://onlinejudge.u-aizu.ac.jp/challenges/search/titles/0718) | Move Balls      | [AC](https://onlinejudge.u-aizu.ac.jp/status/users/S_Seito/submissions/1/0718/judge/6928419/C%23) | 2022-09-30 |
